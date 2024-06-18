417458f0fddafcf882c607e339529a3f9230466e
Junhao Jerry Zhang <jerry_mountain@hotmail.com>
2024-06-17 23:21:25 -0500

    water design

--- objects/water.js
+++ objects/water.js
@@ -1,15 +1,48 @@
 class Water {
     constructor(points) {
         this.points = points;
+
+        let cx = 0, cy = 0;
+        for (const [x, y] of points) {
+            cx += x;
+            cy += y;
+        }
+        cx /= points.length;
+        cy /= points.length;
+
+        this.render = [];
+        for (const [x, y] of points) {
+            this.render.push([x - cx, y - cy]);
+        }
+
+        this.cx = cx;
+        this.cy = cy;
+
+        this.startColor = color(61, 137, 255);
+        this.toColor = color(0, 73, 186);
     }
 
     draw() {
         fill(40, 150, 255);
-        beginShape();
-        for (const [x, y] of this.points) {
-            vertex(x, y);
+        push();
+        translate(this.cx, this.cy);
+        scale(1);
+        for (let i = -1; i < 4; i++) {
+            let c;
+            if (i == -1) {
+                c = color(255, 247, 128);
+            } else {
+                c = lerpColor(this.startColor, this.toColor, i / 4);
+            }
+            fill(c);
+            beginShape();
+            for (const [x, y] of this.render) {
+                vertex(x, y);
+            }
+            endShape();
+            scale(0.9);
         }
-        endShape();
+        pop();
     }
 
     collide(obj) {
