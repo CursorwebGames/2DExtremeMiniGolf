7cffa86016c0c6ed21da6c70d0e05c107ca00944
Junhao Jerry Zhang <jerry_mountain@hotmail.com>
2024-06-20 19:51:37 -0500

    Revert "fixed sand code"
    This reverts commit 795317d525b87df4d48c5f5a6273e0299e4096b4.

--- src/objects/sand.js
+++ src/objects/sand.js
@@ -26,6 +26,8 @@ class Sand {
 
     draw() {
         push();
+
+        push();
         strokeWeight(16);
         stroke(this.to);
         fill(255, 247, 128);
@@ -35,6 +37,42 @@ class Sand {
         }
         endShape(CLOSE);
         pop();
+
+        beginClip();
+        strokeWeight(16);
+        stroke(0);
+        beginShape();
+        for (const [x, y] of this.points) {
+            vertex(x, y);
+        }
+        endShape();
+        endClip();
+
+        // push();
+        // noFill();
+        // strokeWeight(16);
+        // // todo: introduce variables
+        // for (let i = 0; i < 4; i++) {
+        //     const c = lerpColor(this.from, this.to, i / 4);
+        //     stroke(c);
+        //     for (let y = this.miny + i * 16; y < this.maxy; y += 16 * 5) {
+        //         beginShape();
+        //         let deltaY = 1;
+        //         for (let x = this.minx; x < this.maxx; x += 80) {
+        //             vertex(x, y);
+        //             bezierVertex(
+        //                 x + 40, y + deltaY * 10,
+        //                 x + 40, y + deltaY * 10,
+        //                 x + 80, y
+        //             );
+        //             deltaY *= -1;
+        //         }
+        //         endShape();
+        //     }
+        // }
+        // pop();
+
+        pop(); // pop all
     }
 
     collide(ball) {
