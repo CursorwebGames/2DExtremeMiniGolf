947b1b5006cc5ee1ef27bfd4cb9844341c9a4450
Junhao Zhang <jerry_mountain@hotmail.com>
2025-04-07 23:22:08 -0500

    fix building bug

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
         rollupOptions: {
             input: {
                 main: resolve(__dirname, "index.html"),
-                useful: resolve(__dirname, "useful/index.html"),
+                useful: resolve(__dirname, "level-editor/index.html"),
             },
             /*
             output: {
